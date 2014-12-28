a5c065910541d292edb39666ace9d6ed86efd478
Nycto <James@RoundEights.com>
2014-12-28 15:19:43 -0800

    Add Points.slice

--- src/delaunay/Points.hx
+++ src/delaunay/Points.hx
@@ -67,5 +67,59 @@ abstract Points<T: DhxPoint>( Array<T> ) {
     @:arrayAccess public inline function get( index: Int ): T {
         return this[index];
     }
+
+    /** Provides access to a part of this array */
+    public inline function slice( start: Int, length: Int ): Slice<T> {
+        if ( start < 0 || start >= this.length ) {
+            throw "Slice starting index out of bounds: " + start;
+        }
+        else if ( start + length > this.length ) {
+            throw "Slice length out of bounds: " + length;
+        }
+        return new SliceData(this, start, length);
+    }
+}
+
+/** The data representing a slice */
+private class SliceData<T: DhxPoint> {
+    public var data(default, null): Array<T>;
+    public var start(default, null): Int;
+    public var end(default, null): Int;
+    public function new ( data: Array<T>, start: Int, length: Int ) {
+        this.data = data;
+        this.start = start;
+        this.end = start + length;
+    }
+}
+
+/** The interface representing a slice of a Point list */
+abstract Slice<T: DhxPoint> ( SliceData<T> ) from SliceData<T> {
+
+    /** Array length */
+    public inline function length(): Int {
+        return this.end - this.start;
+    }
+
+    /** Array accessor */
+    @:arrayAccess public inline function get( index: Int ): T {
+        var actual = this.start + index;
+        if ( index < 0 || actual >= this.end ) {
+            throw "Index out of bounds: " + index;
+        }
+        return this.data[actual];
+    }
+
+    /** Iteration */
+    public function iterator(): Iterator<T> {
+        var offset = this.start;
+        return {
+            hasNext: function (): Bool {
+                return offset < this.end;
+            },
+            next: function (): T {
+                return this.data[offset++];
+            }
+        };
+    }
 }
 
