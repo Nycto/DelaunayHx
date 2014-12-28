958cb89c847096975bd9f72302c2e4ac51f041aa
Nycto <James@RoundEights.com>
2014-12-28 10:59:59 -0800

    Triangulate 0 to 3 points

--- src/delaunay/Triangulate.hx
+++ src/delaunay/Triangulate.hx
@@ -13,8 +13,40 @@ class Triangulate<T: DhxPoint> {
         this.points = points;
     }
 
+    /** Returns a list of edges when there are only three points. */
+    private static function getTrinary<T: DhxPoint>(
+        points: Array<T>
+    ): Array<Edge<T>> {
+        var a = points[0];
+        var b = points[1];
+        var c = points[2];
+
+        if ( Triangle.isTriangle(a, b, c) ) {
+            return [ new Edge<T>(a, b), new Edge<T>(b, c), new Edge<T>(a, c) ];
+        }
+        else {
+            return [ new Edge<T>(a, b), new Edge<T>(b, c) ];
+        }
+    }
+
+    /** Executes a callback for each determined edge */
+    public function getEdges(): Array<Edge<T>> {
+        var nodes = DivideAndConquer.sort( DivideAndConquer.dedupe(points) );
+
+        return switch ( nodes.length ) {
+            case 0: [];
+            case 1: [];
+            case 2: [ new Edge<T>(nodes[0], nodes[1]) ];
+            case 3: getTrinary(nodes);
+            case _: [];
+        }
+    }
+
     /** Executes a callback for each determined edge */
     public function eachEdge( callback: T -> T -> Void ): Void {
+        for ( edge in getEdges() ) {
+            callback( edge.one, edge.two );
+        }
     }
 
 }
---
+++ test/TriangulateTest.hx
@@ -0,0 +1,70 @@
+package;
+
+import delaunay.Triangulate;
+import delaunay.RealPoint;
+import delaunay.Edge;
+import massive.munit.Assert;
+import haxe.PosInfos;
+
+class TriangulateTest {
+
+    private function p ( x: Float, y: Float ): RealPoint {
+        return new RealPoint(x, y);
+    }
+
+    private function e (
+        x1: Float, y1: Float,
+        x2: Float, y2: Float
+    ): Edge<RealPoint> {
+        return new Edge( p(x1, y1), p(x2, y2) );
+    }
+
+    private function assertEdges (
+        edges: Array<Edge<RealPoint>>,
+        ?points: Array<RealPoint>,
+        ?info: PosInfos
+    ): Void {
+        if ( points == null ) {
+            points = new Array<RealPoint>();
+            for ( edge in edges ) {
+                points.push( edge.one );
+                points.push( edge.two );
+            }
+        }
+        Helper.arrayEquals( edges, new Triangulate(points).getEdges(), info );
+    }
+
+    @Test public function testEmptyPoints():Void {
+        assertEdges( [] );
+    }
+
+    @Test public function testOnePoint():Void {
+        assertEdges( [], [ p(1, 5) ] );
+    }
+
+    @Test public function testTwoPoints():Void {
+        assertEdges([ e(1, 5,  2, 5) ]);
+    }
+
+    @Test public function testThreePointsInATriangle():Void {
+        assertEdges([ e(0, 0,  5, 5), e(5, 5,  10, 0), e(10, 0,  0, 0) ]);
+    }
+
+    @Test public function testThreePointsInALine():Void {
+        assertEdges(
+            [ e(0, 0,  5, 5), e(5, 5,  10, 10) ],
+            [ p(0, 0), p(5, 5), p(10, 10) ]
+        );
+
+        assertEdges(
+            [ e(0, 0,  5, 5), e(5, 5,  10, 10) ],
+            [ p(0, 0), p(10, 10), p(5, 5) ]
+        );
+
+        assertEdges(
+            [ e(0, 0,  5, 5), e(5, 5,  10, 10) ],
+            [ p(5, 5), p(0, 0), p(10, 10) ]
+        );
+    }
+}
+
