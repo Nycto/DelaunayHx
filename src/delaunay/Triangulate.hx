d4e5a1c53721ca689f767b87e2d6c6f315e7d483
Nycto <James@RoundEights.com>
2014-12-30 09:14:34 -0800

    Add split and merge structure

--- src/delaunay/Triangulate.hx
+++ src/delaunay/Triangulate.hx
@@ -15,7 +15,7 @@ class Triangulate<T: DhxPoint> {
 
     /** Returns a list of edges when there are only three points. */
     private static function getTrinary<T: DhxPoint>(
-        points: Points<T>
+        points: Slice<T>
     ): Array<Edge<T>> {
         var a = points[0];
         var b = points[1];
@@ -29,16 +29,36 @@ class Triangulate<T: DhxPoint> {
         }
     }
 
+    /** Merges together sets of edges */
+    private static function merge<T: DhxPoint>(
+        left: Array<Edge<T>>, right: Array<Edge<T>>
+    ): Array<Edge<T>> {
+        return [];
+    }
+
+    /** Triangulates the edges for a list of points */
+    private static function calculate<T: DhxPoint>(
+        points: Slice<T>
+    ): Array<Edge<T>> {
+        return switch ( points.length() ) {
+            case 0: throw "Can not triangulate without any points";
+            case 1: throw "Can not triangulate with only one point";
+            case 2: [ new Edge<T>(points[0], points[1]) ];
+            case 3: getTrinary(points);
+            case _: merge(
+                calculate(points.splitLeft()),
+                calculate(points.splitRight())
+            );
+        }
+    }
+
     /** Executes a callback for each determined edge */
     public function getEdges(): Array<Edge<T>> {
-        var nodes = new Points(points);
-
-        return switch ( nodes.length() ) {
+        var slice: Slice<T> = points;
+        return switch ( slice.length() ) {
             case 0: [];
             case 1: [];
-            case 2: [ new Edge<T>(nodes[0], nodes[1]) ];
-            case 3: getTrinary(nodes);
-            case _: [];
+            case _: calculate(slice);
         }
     }
 
