c645f935a8f6d112cdff5e4c446014eabc56e25a
Nycto <James@RoundEights.com>
2015-01-04 15:37:48 -0800

    Clean up HashSet tests

--- test/SetTest.hx
+++ test/SetTest.hx
@@ -3,13 +3,29 @@ package;
 import delaunay.Set;
 import massive.munit.Assert;
 
+private typedef Id = { id: Int };
+private typedef IdValue = { id: Int, value: Int };
+
 class SetTest {
 
+    private static function hash( value: Id ) {
+        return value.id;
+    }
+
+    private static function idEqual( a: Id, b: Id ) {
+        return a.id == b.id;
+    }
+
+    private static function equal( a: IdValue, b: IdValue ) {
+        return a.value == b.value;
+    }
+
+    private static function compare( a: IdValue, b: IdValue ) {
+        return a.value == b.value ? 0 : (a.value < b.value ? -1 : 1);
+    }
+
     @Test public function testBasicFunctionality():Void {
-        var hashset = new Set<{ id: Int }>(
-            function ( obj ) { return obj.id; },
-            function ( a, b ) { return a.id == b.id; }
-        );
+        var hashset = new Set<Id>( hash, idEqual );
 
         Assert.isFalse( hashset.contains({ id: 50 }) );
         hashset.add({ id: 50 });
@@ -24,92 +40,75 @@ class SetTest {
     }
 
     @Test public function testHashCodeConflicts():Void {
-        var hashset = new Set<{ hash: Int, value: Int }>(
-            function ( obj ) { return obj.hash; },
-            function ( a, b ) { return a.value == b.value; }
-        );
+        var hashset = new Set<IdValue>( hash, equal );
 
-        Assert.isFalse( hashset.contains({ hash: 20, value: 50 }) );
-        Assert.isFalse( hashset.contains({ hash: 20, value: 90 }) );
+        Assert.isFalse( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isFalse( hashset.contains({ id: 20, value: 90 }) );
 
-        hashset.add({ hash: 20, value: 50 });
-        Assert.isTrue( hashset.contains({ hash: 20, value: 50 }) );
-        Assert.isFalse( hashset.contains({ hash: 20, value: 90 }) );
+        hashset.add({ id: 20, value: 50 });
+        Assert.isTrue( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isFalse( hashset.contains({ id: 20, value: 90 }) );
 
-        hashset.add({ hash: 20, value: 90 });
-        Assert.isTrue( hashset.contains({ hash: 20, value: 50 }) );
-        Assert.isTrue( hashset.contains({ hash: 20, value: 90 }) );
+        hashset.add({ id: 20, value: 90 });
+        Assert.isTrue( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 90 }) );
     }
 
     @Test public function testRemove():Void {
-        var hashset = new Set<{ hash: Int, value: Int }>(
-            function ( obj ) { return obj.hash; },
-            function ( a, b ) { return a.value == b.value; }
-        );
+        var hashset = new Set<IdValue>( hash, equal );
 
-        hashset.add({ hash: 20, value: 50 });
-        hashset.add({ hash: 20, value: 90 });
-        hashset.add({ hash: 40, value: 130 });
+        hashset.add({ id: 20, value: 50 });
+        hashset.add({ id: 20, value: 90 });
+        hashset.add({ id: 40, value: 130 });
 
-        Assert.isTrue( hashset.contains({ hash: 20, value: 50 }) );
-        Assert.isTrue( hashset.contains({ hash: 20, value: 90 }) );
-        Assert.isTrue( hashset.contains({ hash: 40, value: 130 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 90 }) );
+        Assert.isTrue( hashset.contains({ id: 40, value: 130 }) );
 
-        hashset.remove({ hash: 20, value: 50 });
-        Assert.isFalse( hashset.contains({ hash: 20, value: 50 }) );
-        Assert.isTrue( hashset.contains({ hash: 20, value: 90 }) );
-        Assert.isTrue( hashset.contains({ hash: 40, value: 130 }) );
+        hashset.remove({ id: 20, value: 50 });
+        Assert.isFalse( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 90 }) );
+        Assert.isTrue( hashset.contains({ id: 40, value: 130 }) );
 
-        hashset.remove({ hash: 40, value: 130 });
-        Assert.isFalse( hashset.contains({ hash: 20, value: 50 }) );
-        Assert.isTrue( hashset.contains({ hash: 20, value: 90 }) );
-        Assert.isFalse( hashset.contains({ hash: 40, value: 130 }) );
+        hashset.remove({ id: 40, value: 130 });
+        Assert.isFalse( hashset.contains({ id: 20, value: 50 }) );
+        Assert.isTrue( hashset.contains({ id: 20, value: 90 }) );
+        Assert.isFalse( hashset.contains({ id: 40, value: 130 }) );
     }
 
     @Test public function testIteration():Void {
-        var hashset = new Set<{ hash: Int, value: Int }>(
-            function ( obj ) { return obj.hash; },
-            function ( a, b ) { return a.value == b.value; }
-        );
+        var hashset = new Set<IdValue>( hash, equal );
 
-        hashset.add({ hash: 20, value: 50 });
-        hashset.add({ hash: 20, value: 90 });
-        hashset.add({ hash: 40, value: 130 });
-        hashset.add({ hash: 20, value: 50 });
+        hashset.add({ id: 20, value: 50 });
+        hashset.add({ id: 20, value: 90 });
+        hashset.add({ id: 40, value: 130 });
+        hashset.add({ id: 20, value: 50 });
 
-        var result = [];
+        var result: Array<IdValue> = [];
         for ( value in hashset ) {
             result.push( value );
         }
 
         Helper.unsortedArrayEqualsUsing(
             [
-                { hash: 20, value: 50 },
-                { hash: 20, value: 90 },
-                { hash: 40, value: 130 }
+                { id: 20, value: 50 },
+                { id: 20, value: 90 },
+                { id: 40, value: 130 }
             ],
             result,
-            function (a, b) { return a.value < b.value ? -1 : 1; },
-            function (a, b) { return a.value == b.value; }
+            compare
         );
     }
 
     @Test public function testEmptyIteration():Void {
-        var hashset = new Set<{ hash: Int, value: Int }>(
-            function ( obj ) { return obj.hash; },
-            function ( a, b ) { return a.value == b.value; }
-        );
+        var hashset = new Set<IdValue>( hash, equal );
 
         var result = [];
         for ( value in hashset ) {
             result.push( value );
         }
 
-        Helper.arrayEqualsUsing(
-            [],
-            hashset.toArray(),
-            function (a, b) { return a.value == b.value; }
-        );
+        Helper.arrayEqualsUsing( [], result, equal );
     }
 }
 
