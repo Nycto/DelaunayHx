5fb5fc29cc6d925c520f17b6e7135159245136e2
Nycto <James@RoundEights.com>
2015-01-11 22:36:54 -0800

    Render 100 point graph in sample

--- sample/src/Main.hx
+++ sample/src/Main.hx
@@ -1,9 +1,34 @@
 package;
 
+import delaunay.RealPoint;
+import delaunay.Triangulate;
+
 class Main {
 
     static function main() {
-        trace("Hello");
+        for (
+            canvasNode in
+            js.Browser.document.querySelectorAll("canvas.delaunay")
+        ) {
+            var canvas = cast( canvasNode, js.html.CanvasElement );
+
+            var nodes = new Triangulate([]);
+            for ( i in 0...100 ) {
+                nodes.add( new RealPoint(
+                    Std.random(canvas.clientWidth),
+                    Std.random(canvas.clientHeight)
+                ) );
+            }
+
+            var context = canvas.getContext2d();
+            nodes.eachEdge(function( a, b ) {
+                trace(a + " -> " + b);
+                context.beginPath();
+                context.moveTo( a.getX(), a.getY() );
+                context.lineTo( b.getX(), b.getY() );
+                context.stroke();
+            });
+        }
     }
 }
 
